1b7e2dee4264b76551ff397a15baa37e0791648b
Juan Gadpen <juangadpen@hotmail.com>
2023-11-08 17:48:53 -0300

    Simple changes

--- frontend/src/components/employee/AddEmployee.js
+++ frontend/src/components/employee/AddEmployee.js
@@ -30,7 +30,7 @@ const AddEmployee = () => {
   };
   return (
     <div className="col-sm-8 py-2 px-5">
-      <h2 className="mt-5">Add Student</h2>
+      <h2 className="mt-5">Add Employee</h2>
       <form onSubmit={(e) => saveEmployee(e)}>
         <div className="input-group mb-5">
           <label className="input-group-text" htmlFor="firstName">
--- frontend/src/components/employee/EditEmployee.js
+++ frontend/src/components/employee/EditEmployee.js
@@ -49,7 +49,7 @@ const EditEmployee = () => {
 
   return (
     <div className="col-sm-8 py-2 px-5">
-      <h2 className="mt-5">Add Student</h2>
+      <h2 className="mt-5">Edit Employee</h2>
       <form onSubmit={(e) => updateEmployee(e)}>
         <div className="input-group mb-5">
           <label className="input-group-text" htmlFor="firstName">
